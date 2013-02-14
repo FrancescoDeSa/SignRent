8f8382ec34c39f3ba706bc31bd03cb6d4cba1e05
FrancescoDeSa <francesco.desantis.1803@gmail.com>
2013-02-14 14:39:14 +0100

    shaping

--- SignRent/src/com/github/FrancescoDeSa/SignListener.java
+++ SignRent/src/com/github/FrancescoDeSa/SignListener.java
@@ -1,9 +1,14 @@
 package com.github.FrancescoDeSa;
 
+//import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+//import org.bukkit.inventory.ItemStack;
 
 public class SignListener implements Listener {
 
@@ -14,5 +19,20 @@ public class SignListener implements Listener {
         giocatore.sendMessage("Complimenti,"+nome+", hai piazzato un cartello!");
         String linea = event.getLine(0);
         giocatore.sendMessage("E nella prima riga hai messo \""+linea+"\"!!!");
+    }
+    
+    @EventHandler
+    public void cartelloCliccato(PlayerInteractEvent event){
+    	Player player = event.getPlayer();
+    	if(event.hasBlock()){
+    		Block block = event.getClickedBlock();
+            Action azione = event.getAction();
+            if(azione == Action.RIGHT_CLICK_BLOCK){
+            	int tipo = block.getType().getId();
+            	if(tipo == 63 || tipo == 68){
+            		player.sendMessage("Hey, stai interagendo con un cartello!");
+            	}
+            }
+    	}
     }
 }
